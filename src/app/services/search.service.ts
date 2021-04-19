b99aa88e7132fa231f15109f00a008909f915039
lukeyd <luke6534@gmail.com>
2021-04-19 15:24:14 -0500

    make search request in service
    make search request in service

--- src/app/services/search.service.ts
+++ src/app/services/search.service.ts
@@ -1,9 +1,25 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { User } from '../interfaces/user';
+import {HttpClient, HttpParams} from '@angular/common/http';
+import { SearchResults } from '../interfaces/search-results';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class SearchService {
+  // tslint:disable-next-line:no-inferrable-types
+  private url: string = 'https://api.github.com/search';
+  constructor(private http: HttpClient) { }
 
-  constructor() { }
+
+  public User(user: string): Observable<SearchResults> {
+    const userUrl: string = this.url + '/users';
+
+    const p = new HttpParams().set('q', user);
+
+    return this.http.get<SearchResults>(userUrl, {params: p});
+
+  }
 }
